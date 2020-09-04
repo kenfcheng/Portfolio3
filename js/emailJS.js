107aa87f2968797bd11ad7b9b8c6a3f7423dc14f
Ken Cheng <kenfcheng@gmail.com>
2020-09-04 17:45:31 -0400

    EmailJS script added

---
+++ js/emailJS.js
@@ -0,0 +1,14 @@
+(function () {
+  emailjs.init("service_vekm7fg");
+})();
+
+window.onload = function () {
+  document
+    .getElementById("contact-form")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      // generate the contact number value
+      this.contact_number.value = (Math.random() * 100000) | 0;
+      emailjs.sendForm("contact_service", "contact_form", this);
+    });
+};
